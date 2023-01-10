d20abbbb0d8faf58f8959f5bbfd5d0dcc8db4b47
A-baoYang <jiunyi.yang.abao@gmail.com>
2023-01-10 09:04:40 +0000

    README.md

--- api/bloom.py
+++ api/bloom.py
@@ -14,14 +14,13 @@ class BLOOM:
             input_ids,
             max_length=1024,
             # num_beams=5,
-            no_repeat_ngram_size=2,
+            # no_repeat_ngram_size=2,
             # num_return_sequences=3,
             # early_stopping=True,
-            # do_sample=True,
-            # temperature=0.7,
-            # top_k=0,
-            # top_p=0.92,
-            # max_tokens=self.max_tokens,
+            do_sample=True,
+            temperature=0.7,
+            top_k=0,
+            # top_p=0.92
         )
         response = self.tokenizer.decode(greedy_output[0], skip_special_tokens=True)
         return response.strip()
